07d7e93b5001e5ffc26419232fa2759d1e46edc0
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-04-02 15:07:42 +0700

    angularJsBasic

---
+++ MyVocal.Web/Scripts/spa/service.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myService = angular.module('myService', []);
+
+myService.controller('serviceController', serviceController);
+
+myService.service('checkNumber', checkNumber);
+serviceController.$inject=['$scope','checkNumber']
+
+function serviceController($scope, checkNumber) {
+
+    $scope.CheckNumber = function () {
+        
+        $scope.message=checkNumber.check($scope.num);
+    };
+
+}
+
+function checkNumber() {
+    return {
+        check:check
+    }
+    function check(number) {
+        if (number % 2 == 0) {
+            return 'This is even';
+        } else {
+            return 'This is odd';
+        }
+    }
+}
