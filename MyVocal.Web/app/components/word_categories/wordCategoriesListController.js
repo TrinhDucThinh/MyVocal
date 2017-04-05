4f900c1bc6026eaee138aab72d478269bdf8cf8b
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-04-05 23:11:49 +0700

    add noiticationService for project

--- MyVocal.Web/app/components/word_categories/wordCategoriesListController.js
+++ MyVocal.Web/app/components/word_categories/wordCategoriesListController.js
@@ -1,17 +1,18 @@
 ﻿/// <reference path="E:\Document\Đồ án\Project\Git\MyVocal\MyVocal.Web\Assets/admin/libs/angular/angular.js" />
 (function (app) {
     app.controller('wordCategoriesListController', wordCategoriesListController);
-    wordCategoriesListController.$inject = ['$scope', 'apiService'];
-    function wordCategoriesListController($scope, apiService) {
+    wordCategoriesListController.$inject = ['$scope', 'apiService', 'notificationService'];
+    function wordCategoriesListController($scope, apiService, notificationService) {
         $scope.wordCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
         $scope.getWordCategories = getWordCategories;
         $scope.keyword = '';
-
+        var isSearch=false;
         $scope.search = search;
-        
+
         function search() {
+            isSearch = true;
             getWordCategories();
         }
 
@@ -19,12 +20,21 @@
             page = page || 0;
             var config = {
                 params: {
-                    keyword:$scope.keyword,
-                    page:page,
-                    pageSize:2
+                    keyword: $scope.keyword,
+                    page: page,
+                    pageSize: 2
                 }
             }
             apiService.get('/api/WordCategory/getall', config, function (result) {
+                if (isSearch) {
+                    if (result.data.TotalCount == 0) {
+                        notificationService.displayWarning('Không có bản ghi nào được tìm thấy.'); 
+                    } else {
+                        notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi.');
+                    }
+                    isSearch = false;
+                }
+
                 $scope.wordCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.totalCount = result.data.TotalCount;
@@ -33,8 +43,6 @@
                 console.log('Load word categories failure');
             });
         }
-
         $scope.getWordCategories();
-        
     }
 })(angular.module('myvocal.word_categories'));
