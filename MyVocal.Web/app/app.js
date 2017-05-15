cd150f3e258188b2a301ee742b6363e84a302f60
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-05-15 18:01:55 +0700

    add report

--- MyVocal.Web/app/app.js
+++ MyVocal.Web/app/app.js
@@ -13,14 +13,33 @@
         ]).config(config);
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    //function config($stateProvider, $urlRouterProvider) {
+    //    $stateProvider
+    //        .state('base', {
+    //            url: '',
+    //            templateUrl: '/app/shared/views/baseView.html',
+    //            abstract: true
+    //        })
+    //        .state('login', {
+    //            url: "/login",
+    //            templateUrl: "/app/components/login/loginView.html",
+    //            controller: "loginController"
+    //        })
+    //        .state('home', {
+    //            url: "/admin",
+    //            parent: 'base',
+    //            templateUrl: "/app/components/home/homeView.html",
+    //            controller: "homeController"
+    //        });
+    //    $urlRouterProvider.otherwise('/admin');
+    //}
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('base', {
                 url: '',
                 templateUrl: '/app/shared/views/baseView.html',
                 abstract: true
-            })
-            .state('login', {
+            }).state('login', {
                 url: "/login",
                 templateUrl: "/app/components/login/loginView.html",
                 controller: "loginController"
@@ -31,6 +50,37 @@
                 templateUrl: "/app/components/home/homeView.html",
                 controller: "homeController"
             });
-        $urlRouterProvider.otherwise('/admin');
+        $urlRouterProvider.otherwise('/login');
     }
 })();
+
+
+//(function () {
+//    angular.module('tedushop',
+//        ['tedushop.products',
+//         'tedushop.product_categories',
+//         'tedushop.common'])
+//        .config(config);
+
+//    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+//    function config($stateProvider, $urlRouterProvider) {
+//        $stateProvider
+//            .state('base', {
+//                url: '',
+//                templateUrl: '/app/shared/views/baseView.html',
+//                abstract: true
+//            }).state('login', {
+//                url: "/login",
+//                templateUrl: "/app/components/login/loginView.html",
+//                controller: "loginController"
+//            })
+//            .state('home', {
+//                url: "/admin",
+//                parent: 'base',
+//                templateUrl: "/app/components/home/homeView.html",
+//                controller: "homeController"
+//            });
+//        $urlRouterProvider.otherwise('/login');
+//    }
+//})();
--- MyVocal.Web/app/components/login/loginController.js
+++ MyVocal.Web/app/components/login/loginController.js
@@ -1,22 +1,9 @@
 ﻿(function (app) {
-    app.controller('loginController', ['$scope', 'loginService', '$injector', 'notificationService',
-        function ($scope, loginService, $injector, notificationService) {
-
-            $scope.loginData = {
-                userName: "",
-                password: ""
-            };
-
-            $scope.loginSubmit = function () {
-                loginService.login($scope.loginData.userName, $scope.loginData.password).then(function (response) {
-                    if (response != null && response.error != undefined) {
-                        notificationService.displayError("Đăng nhập không đúng.");
-                    }
-                    else {
-                        var stateService = $injector.get('$state');
-                        stateService.go('word_categories');
-                    }
-                });
-            }
-        }]);
+    app.controller('loginController', loginController);
+    loginController.$inject = ['$scope', '$state'];
+    function loginController($scope, $state) {
+        $scope.loginSubmit = function () {
+            $state.go('home');
+        }
+    }
 })(angular.module('myvocal'));
