753a0253f4bfcd0c9063e0eb2a10c884beffc030
Trinh Duc Thinh <tdthinh1995@gmail.com>
2017-05-21 00:25:50 +0700

    not admin

--- MyVocal.Web/App_Start/WebApiConfig.cs
+++ MyVocal.Web/App_Start/WebApiConfig.cs
@@ -16,11 +16,11 @@ namespace MyVocal.Web
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
-           new DefaultContractResolver { IgnoreSerializableAttribute = true };
+           // config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+           //new DefaultContractResolver { IgnoreSerializableAttribute = true };
 
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
